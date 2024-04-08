bf93d6d13baf4d406d754ba1803f07afaa2cd80d
AthenaSui <wangmz29@163.com>
2024-04-08 20:06:55 +0800

    1

--- data/sql/db-world/2024_04_07_02_innkeeper.sql
+++ data/sql/db-world/2024_04_07_02_innkeeper.sql
@@ -1,3 +1,1 @@
-UPDATE `guild_house_spawns` SET `posX`=16221.91, `posY`=16288.54, `posZ`=13.17, `orientation`=4.65 WHERE `id`=10;
-
-UPDATE `creature` SET `position_x`=16221.91, `position_y`=16288.54, `position_z`=13.17, `orientation`=4.65 WHERE `id1`=500032;
+UPDATE `creature` SET `unit_flags`=128 WHERE `id1` in (10367, 9856, 8719);
